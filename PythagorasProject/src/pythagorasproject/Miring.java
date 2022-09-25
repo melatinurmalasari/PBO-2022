b6774a4df83d994cbbf0aefbd846706f449baf3c
Melati Nur Malasari <90134551+melatinurmalasari@users.noreply.github.com>
2022-09-25 13:28:26 +0700

    PythagorasProject (Modul 5)

---
+++ PythagorasProject/src/pythagorasproject/Miring.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pythagorasproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melati Nur Malasari
+ */
+public class Miring {
+    int a;
+    int b;
+    double c = 0;
+    
+    void input(){
+        
+        System.out.println("");
+        System.out.println("Menentukan sisi miring segitiga");
+        
+        
+        Scanner data = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Masukan sisi [a] : ");
+            a = data.nextInt();
+           
+            
+            System.out.println("Masukan sisi [b] : ");
+            b = data.nextInt();
+            break;
+            
+        }
+    }
+    
+    void sisiMiring(){
+        this.c = Math.sqrt(Math.pow(a, 2) + (Math.pow(b, 2)));
+        System.out.println("Sisi miringnya [c] adalah : " + c);
+    }
+    
+}
---
+++ PythagorasProject/src/pythagorasproject/sikuSiku.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pythagorasproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melati Nur Malasari
+ */
+public class sikuSiku {
+    int a;
+    int c;
+    double b = 0;
+    
+    void input(){
+        
+        System.out.println("");
+        System.out.println("Menentukan sisi siku-siku segitiga");
+        
+        Scanner data = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Masukan sisi [a] : ");
+            a = data.nextInt();
+           
+            
+            System.out.println("Masukan sisi miring [c] : ");
+            c = data.nextInt();
+            break;
+            
+        }
+    }
+    
+    void sisiSiku(){
+        this.b = Math.sqrt(Math.pow(c, 2) - (Math.pow(a, 2)));
+        System.out.println("Sisi siku-siku adalah : " + b);
+    }
+}
